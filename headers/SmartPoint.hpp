e20f79a79ba6918cc1c215d0b9c332783fb6cbb1
Anonimus Sergey <sergey3010.01@mail.ru>
2025-11-11 00:06:42 +0300

    Point implementation

--- headers/SmartPoint.hpp
+++ headers/SmartPoint.hpp
@@ -1,17 +1,58 @@
 #pragma once
-
-
 #include <mutex>
+#include <math.h>
+template<typename T>
+class CartesianPoint
+{
+public:
+    // constuctors/destructors
+    CartesianPoint(T x, T y, T z): x(x), y(y), z(z){}
+    CartesianPoint(const CartesianPoint<T>& other): x(other.x), y(other.y), z(other.z){}
+    ~CartesianPoint(){}
+    //operators
+    bool operator==(const CartesianPoint<T>& other) const { return x == other.x && y == other.y && z == other.z; }
+    CartesianPoint<T> operator+(const CartesianPoint<T>& other) const { return CartesianPoint<T>(x + other.x, y + other.y, z + other.z); }
+    CartesianPoint<T> operator-(const CartesianPoint<T>& other) const { return CartesianPoint<T>(x - other.x, y - other.y, z - other.z); }
+    CartesianPoint<T> operator*(const CartesianPoint<T>& other) const { return CartesianPoint<T>(x * other.x, y * other.y, z * other.z); }
+    ostream operator<<(ostream& os) const { return os << x << " " << y << " " << z; }
+    
+    x = r*sin(alpha)*sin(teta);
+    y = r * cos(alpha)*sin(teta);
+    z = r*cos(teta);
+    PolarPoint<T> toPolar(){T r = std::sqrt(x^2 + y^2 + z^2); };
+    
+public:
+    T x, y, z;
+};
 
-template<typename T, typename PT> 
+template <typename T>
+class PolarPoint
+{
+public:
+    PolarPoint(T r, T alpha, T teta): r(r), alpha(alpha), teta(teta){}
+    ~PolarPoint(){}
+    CartesianPoint<T> toCartesian();
+public:
+    T r, alpha, teta;
+};
+
+
+
+template<typename T> 
 class SmartPoint
 {
     mutable std::mutex mtx;
+
 public:
-    SmartPoint(T x,T y, T z): x(x), y(y), z(z){}
+    SmartPoint(CartesianPoint<T> c): c(c){
+
+    }
+    SmartPoint(PolarPoint<T> p): p(p){}
+
 private:
-    T x, y, z;
-    float r, alpha, teta;
+    PolarPoint<T> p;
+    CartesianPoint<T> c;
+
     SmartPoint& operator=(const SmartPoint& p);
 };
 
